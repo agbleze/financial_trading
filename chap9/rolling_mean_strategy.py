9bb144f519b05e8e836e454a4f4ca63a9709eb7d
agbleze <lagbleze@st.ug.edu.gh>
2023-03-21 23:50:08 +0100

    Update rolling_mean_strategy.py

--- chap9/rolling_mean_strategy.py
+++ chap9/rolling_mean_strategy.py
@@ -1,3 +1,12 @@
 from zipline import run_algorithm
 from zipline.api import order_target_percent, symbol, set_commission
+from zipline.finance.commission import PerTrade
+import pandas as pd
+import pyfolio as pf
+import warnings
+
+
+
+
+
 
